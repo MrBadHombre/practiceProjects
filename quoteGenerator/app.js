ab7fedda82468c442df1caaae26574ea39a4a0b0
Michael <l1ghtdev.88@gmail.com>
2022-10-09 18:37:49 -0700

    add loading icon styling and js functionality

--- quoteGenerator/app.js
+++ quoteGenerator/app.js
@@ -3,28 +3,43 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 let apiQuotes = [];
 
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 //show new quote
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (!quote.author) {
     authorText.textContent = 'Unknown';
   } else {
-    if (quote.text.length > 120) {
-      quoteText.classList.add('long-quote');
-    } else {
-      quoteText.classList.remove('long-quote');
-    }
     authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
   }
 
   quoteText.textContent = quote.text;
+  complete();
 }
 
 //Get quotes from API
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
