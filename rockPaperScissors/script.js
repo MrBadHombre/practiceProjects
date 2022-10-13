77bc544abfe5d834194a4750b9571750fef57696
Michael <l1ghtdev.88@gmail.com>
2022-10-12 23:25:29 -0700

    Game logic for player and computer selections

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,14 +1,82 @@
-const buttons = document.querySelector(".button");
-const overlay = document.querySelector(".overlay");
-const title = document.querySelector(".title");
-const red = document.querySelector(".overlay");
-const showOverlay = function () {
-  overlay.classList.toggle("hidden");
-};
-const hideOverlay = function () {
-  overlay.classList.toggle("hidden");
-};
+// function playerChoice(playerSelection) {
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+// let computerSelection = Math.floor(Math.random() * 3);
+function winner() {
+  let computerScore = 0;
+  let playerScore = 0;
+  let playerSelection = 1;
+  let computerSelection = Math.floor(Math.random() * 3);
+  if (
+    (playerSelection === 1 && computerSelection === 2) ||
+    (playerSelection === 2 && computerSelection === 3) ||
+    (playerSelection === 3 && computerSelection === 1)
+  ) {
+    computerScore++;
+    console.log('you lose');
+  } else if (playerSelection === computerSelection) {
+    // draw alert
+  } else {
+    playerScore++;
+    console.log('you win');
+  }
+}
 
-buttons.addEventListener("mouseenter", showOverlay);
+rock.addEventListener('click', function () {
+  let computerScore = 0;
+  let playerScore = 0;
+  let playerSelection = 1;
+  let computerSelection = Math.floor(Math.random() * 3) + 1;
+  if (
+    (playerSelection === 1 && computerSelection === 2) ||
+    (playerSelection === 2 && computerSelection === 3) ||
+    (playerSelection === 3 && computerSelection === 1)
+  ) {
+    computerScore++;
+    console.log('you lose');
+    console.log(playerSelection);
+  } else if (playerSelection === computerSelection) {
+    console.log(playerSelection);
+    // draw alert
+  } else {
+    playerScore++;
+    console.log('you win');
 
-buttons.addEventListener("mouseout", hideOverlay);
+    console.log(playerSelection);
+  }
+});
+//   paper.addEventListener('click', function () {
+//     playerSelection = 2;
+//     return playerSelection;
+//   });
+//   scissors.addEventListener('click', function () {
+//     playerSelection = 3;
+//     return playerSelection;
+//   });
+//   if (
+//     (playerSelection === 1 && computerSelection === 2) ||
+//     (playerSelection === 2 && computerSelection === 3) ||
+//     (playerSelection === 3 && computerSelection === 1)
+//   ) {
+//     computerScore++;
+//     console.log('you lose');
+//   } else if (playerSelection === computerSelection) {
+//     // draw alert
+//   } else {
+//     playerScore++;
+//   }
+//   console.log(playerSelection);
+// }
+
+// computer selection
+
+// compare to determine winner
+
+// add score to total
+
+// socre > 5 play again
+
+// score = 5 determine winner
+
+// ask to play again
