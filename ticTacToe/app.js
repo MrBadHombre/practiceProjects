612b5f6f2c6db8764f1b987963b21cf0d608fbe0
Michael <l1ghtdev.88@gmail.com>
2022-11-19 12:43:09 -0800

    grid layout and create array from grid items

---
+++ ticTacToe/app.js
@@ -0,0 +1,37 @@
+// create grid -----
+// style grid and page -----
+// store each grid item in an array -----
+// creat click event that changes color of grid square on click
+// add X or O to grid squares after clicking
+// make function to create players
+// create game function
+// refactor code
+let player;
+let spacesArray = [];
+const gameSpaces = document.querySelectorAll('.spaces');
+
+for (let i = 0; i < gameSpaces.length; i++) {
+  spacesArray.push([i]);
+}
+
+console.log(gameSpaces);
+
+gameSpaces.addEventListener('click', function color(event) {
+  gameSpaces.style.backgroundColor = 'red';
+});
+
+function playerCreation() {
+  if (!player) {
+    player = {
+      playerSymbol: 'x',
+      playerName: 'Player1',
+    };
+  } else {
+    player = {
+      playerSymbol: 'o',
+      playerName: 'Player 2',
+    };
+  }
+}
+
+playerCreation();
