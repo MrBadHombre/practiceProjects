20f658ee0b03a12cda3cac8a6efb7074919a2ccb
Michael Margrave <l1ghtdev.88@gmail.com>
2022-11-23 13:11:03 -0800

    add functionality so game spaces can only be selected one time

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -10,30 +10,56 @@
 // refactor code
 let player;
 let spacesArray = [];
+let activePlayer = 'player1';
+let player1Score = [];
+let player2Score = [];
 const gameSpaces = document.querySelectorAll('.spaces');
 
+// spacesArray = Array.from(gameSpaces);
+// console.log(spacesArray);
 for (let i = 0; i < gameSpaces.length; i++) {
-  spacesArray.push([i]);
+  spacesArray.push(gameSpaces[i]);
+}
+console.log(spacesArray);
+
+for (let i = 0; i < spacesArray.length; i++) {
+  spacesArray[i].textContent = 'null';
+  spacesArray[i].addEventListener('click', function playerChoice(event) {
+    if (activePlayer == 'player1' && spacesArray[i].textContent == 'null') {
+      spacesArray[i].textContent = 'x';
+      player1Score.push(spacesArray[i]);
+      console.log(player1Score);
+      activePlayer = 'player2';
+    } else if (
+      activePlayer == 'player2' &&
+      spacesArray[i].textContent == 'null'
+    ) {
+      spacesArray[i].textContent = 'o';
+      activePlayer = 'player1';
+    } else {
+      activePlayer = activePlayer;
+    }
+  });
 }
 
-console.log(gameSpaces);
-
-gameSpaces.addEventListener('click', function color(event) {
-  gameSpaces.style.backgroundColor = 'red';
-});
-
-function playerCreation() {
-  if (!player) {
-    player = {
-      playerSymbol: 'x',
-      playerName: 'Player1',
-    };
-  } else {
-    player = {
-      playerSymbol: 'o',
-      playerName: 'Player 2',
-    };
+function winner(player1Score) {
+  console.log(player1Score);
+  if (player1Score.includes(spacesArray[(0, 1, 2)])) {
+    prompt('WINNER');
   }
 }
+// function playerCreation() {
+//   if (!player) {
+//     player = {
+//       playerSymbol: 'x',
+//       playerName: 'Player1',
+//     };
+//   } else {
+//     player = {
+//       playerSymbol: 'o',
+//       playerName: 'Player 2',
+//     };
+//   }
+// }
 
-playerCreation();
+// playerCreation()
