7739b93cfbf890c8281e659f822a74283da92822
Michael Margrave <l1ghtdev.88@gmail.com>
2022-11-30 13:32:23 -0800

    create start and player selection screens

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -8,32 +8,52 @@
 // make function to create players
 // create game function
 // refactor code
+
 let player;
 let spacesArray = [];
 let activePlayer = 'player1';
 let player1Score = [];
 let player2Score = [];
 const gameSpaces = document.querySelectorAll('.spaces');
+const gameBoard = document.querySelector('.gameboard');
+const startButton = document.querySelector('#playGame');
+const playerChoicesGood = document.querySelectorAll('.playerChoicesGood');
+const playerSelectionScreenOne = document.querySelector(
+  '.playerSelectionScreenOne'
+);
 
-// spacesArray = Array.from(gameSpaces);
-// console.log(spacesArray);
-for (let i = 0; i < gameSpaces.length; i++) {
-  spacesArray.push(gameSpaces[i]);
-}
+// start screen
+
+startButton.addEventListener('click', () => {
+  startButton.classList.add('hidden');
+  playerSelectionScreenOne.classList.remove('hidden');
+  // gameboard needs to remove hidden. hidden to create player selection page
+  // gameBoard.classList.remove('hidden');
+});
+
+// player 1 character selection screen
+
+goodGuysArray = Array.from(playerChoicesGood);
+for (let i = 0; i < goodGuysArray.length; i++)
+  goodGuysArray[i].addEventListener('click', () => {
+    let player1 = goodGuysArray[i];
+    console.log(player1);
+    playerSelectionScreenOne.classList.add('hidden');
+  });
+
+// gameboard creation
+spacesArray = Array.from(gameSpaces);
 console.log(spacesArray);
 
 for (let i = 0; i < spacesArray.length; i++) {
-  spacesArray[i].textContent = 'null';
+  spacesArray[i].textContent = '';
   spacesArray[i].addEventListener('click', function playerChoice(event) {
-    if (activePlayer == 'player1' && spacesArray[i].textContent == 'null') {
+    if (activePlayer == 'player1' && spacesArray[i].textContent == '') {
       spacesArray[i].textContent = 'x';
-      player1Score.push(spacesArray[i]);
+      player1Score.push(spacesArray.indexOf(spacesArray[i]));
       console.log(player1Score);
       activePlayer = 'player2';
-    } else if (
-      activePlayer == 'player2' &&
-      spacesArray[i].textContent == 'null'
-    ) {
+    } else if (activePlayer == 'player2' && spacesArray[i].textContent == '') {
       spacesArray[i].textContent = 'o';
       activePlayer = 'player1';
     } else {
@@ -41,25 +61,3 @@ for (let i = 0; i < spacesArray.length; i++) {
     }
   });
 }
-
-function winner(player1Score) {
-  console.log(player1Score);
-  if (player1Score.includes(spacesArray[(0, 1, 2)])) {
-    prompt('WINNER');
-  }
-}
-// function playerCreation() {
-//   if (!player) {
-//     player = {
-//       playerSymbol: 'x',
-//       playerName: 'Player1',
-//     };
-//   } else {
-//     player = {
-//       playerSymbol: 'o',
-//       playerName: 'Player 2',
-//     };
-//   }
-// }
-
-// playerCreation()
